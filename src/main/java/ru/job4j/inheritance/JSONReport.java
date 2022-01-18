1928a0fe37703fc1670bc31505099b848be64882
Den <dezer1993@yandex.ru>
2022-01-18 22:33:50 +0300

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextPeport {
+    @Override
     public String generate(String name, String body) {
         String ln = System.lineSeparator();
         return "{" + ln

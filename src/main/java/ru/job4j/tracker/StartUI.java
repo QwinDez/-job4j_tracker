9d093b788a4c4c67cf08fc02eec16cb60558bf27
Den <dezer1993@yandex.ru>
2022-01-27 17:33:40 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744 #256149]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -27,7 +27,7 @@ public class StartUI {
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
-            } else if (select != 6) {
+            } else if (select == 6) {
                 System.out.println("Пользователь выбрал: " + select);
             } else {
                 run = false;

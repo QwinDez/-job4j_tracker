50325b1761cf1037fa75768bc84b2942d2653911
Den <dezer1993@yandex.ru>
2022-02-03 10:40:06 +0300

    9.2. Тесты вывода на консоль [#33585 #259374]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -121,7 +121,7 @@ public class StartUITest {
                         + "0. Show all items" + ln
                         + "1. Exit" + ln
                         + "=== Show all items ===" + ln
-                        +  one.toString() + ln
+                        +  one + ln
                         + "Menu:" + ln
                         + "0. Show all items" + ln
                         + "1. Exit" + ln
@@ -148,7 +148,7 @@ public class StartUITest {
                         + "0. Find items by name" + ln
                         + "1. Exit" + ln
                         + "=== Find items by name ===" + ln
-                        + one.toString() + ln
+                        + one + ln
                         + "Menu:" + ln
                         + "0. Find items by name" + ln
                         + "1. Exit" + ln
@@ -175,7 +175,7 @@ public class StartUITest {
                         + "0. Find item by id" + ln
                         + "1. Exit" + ln
                         + "=== Find item by id ===" + ln
-                        + one.toString() + ln
+                        + one + ln
                         + "Menu:" + ln
                         + "0. Find item by id" + ln
                         + "1. Exit" + ln

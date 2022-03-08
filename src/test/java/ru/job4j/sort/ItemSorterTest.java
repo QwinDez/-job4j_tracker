e541df73e64fa630ba773d2fc5d17148822227a4
Den <dezer1993@yandex.ru>
2022-03-08 19:16:57 +0300

    0. Сортировка [#10096 #273026]

--- src/test/java/ru/job4j/sort/ItemSorterTest.java
+++ src/test/java/ru/job4j/sort/ItemSorterTest.java
@@ -3,6 +3,7 @@ package ru.job4j.sort;
 import org.junit.Test;
 import ru.job4j.tracker.Item;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -14,35 +15,34 @@ public class ItemSorterTest {
 
     @Test
     public void sortAscItem() {
-        List<Item> items = Arrays.asList(
-                new Item(12, "Alex"),
-                new Item(32, "Ivan"),
-                new Item(21, "Petr")
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Alex"));
+        items.add(new Item("Ivan"));
+        items.add(new Item("Petr"));
+        List<Item> expected = List.of(
+                new Item("Alex"),
+                new Item("Ivan"),
+                new Item("Petr")
         );
         Collections.sort(items, new ItemAscByName());
-        List<Item> expected = Arrays.asList(
-                new Item(12, "Alex"),
-                new Item(32, "Ivan"),
-                new Item(21, "Petr")
-        );
-        assertThat(items.toString(), is(expected.toString()));
+        List<Item> result = items;
+        assertThat(result.toString(), is(expected.toString()));
     }
 
     @Test
     public void sortDescItem() {
-        List<Item> items = Arrays.asList(
-                new Item(12, "Alex"),
-                new Item(32, "Ivan"),
-                new Item(21, "Petr")
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Alex"));
+        items.add(new Item("Ivan"));
+        items.add(new Item("Petr"));
+        List<Item> expected = List.of(
+                new Item("Alex"),
+                new Item("Ivan"),
+                new Item("Petr")
         );
         Collections.sort(items, new ItemDescByName());
-        List<Item> expected = Arrays.asList(
-                new Item(21, "Petr"),
-                new Item(32, "Ivan"),
-                new Item(12, "Alex")
-
-        );
-        assertThat(items.toString(), is(expected.toString()));
+        List<Item> result = items;
+        assertThat(result.toString(), is(expected.toString()));
     }
 
 }
